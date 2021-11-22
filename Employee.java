d7ce255a3b7a271d17dbdfac7c7a54e4f52c521d
jupallypooja <90761281+jupallypooja@users.noreply.github.com>
2021-11-22 22:43:57 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,35 @@
+package com.collection.addressbook;
+
+public class Employee {
+       private int empno;
+	   private String ename;
+	   private int salary;
+
+	   Employee(int empno, String ename, int salary)
+	   {
+	      this.empno = empno;
+	      this.ename = ename;
+	      this.salary = salary;
+	   }
+
+	   public int getEmpno()
+	   {
+	      return empno;
+	   }
+	   public int getSalary()
+	   {
+	      return salary;
+	   }
+	   public String getEname()
+	   {
+	      return ename;
+	   }
+
+	   public String toString()
+	   {
+	      return empno+" "+ename+" "+salary;
+	   }
+	
+}
+
+
